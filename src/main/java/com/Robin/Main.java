577b7bfc3e0ed1a69d38709daf663d27f9a4e0bc
Robin SIngh <robin.122497@gmail.com>
2023-06-03 12:43:50 +0530

    Initial commit

---
+++ src/main/java/com/Robin/Main.java
@@ -0,0 +1,64 @@
+package com.Robin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/customers")
+public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request) {
+        if (customerRepository.findById(id).isPresent()) {
+            Customer customer = customerRepository.getById(id);
+
+            if (request.name != null && !request.name.isEmpty())
+                customer.setName(request.name);
+            if (request.email != null && !request.email.isEmpty())
+                customer.setEmail(request.email);
+            if (request.age != null && request.age > 0)
+                customer.setAge(request.age);
+            customerRepository.save(customer);
+        }
+    }
+}
